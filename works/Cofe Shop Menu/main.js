4b6efc83199bda3f10e3410492e7ecc812e74199
伊藤卓爾 <karambit1341@gmail.com>
2025-09-23 11:17:29 +0900

    css

--- works/Cofe Shop Menu/main.js
+++ works/Cofe Shop Menu/main.js
@@ -1,19 +1,20 @@
 
-/*-----【ページ内スクロール】-----*/
-$(function () {
+$(function(){
 
-  $('.scroll-btn').click(function (e) {
-    const href = $(this).attr('href');
-    // ページ内スクロールの場合のみ処理を実行
-    //href がページ内リンク（#で始まる）であるかをチェック
-    if (href.startsWith('#') && $(href).length) {
-      e.preventDefault(); // デフォルト動作（リンク遷移）を防ぐ
-      $('html, body').animate({ scrollTop: $(href).offset().top }, 500, "swing");
-    }
+  $('a[href^="#"]').click(function(){
+
+    // リンクを取得
+    let href= $(this).attr("href");
+   
+    //  href が "#" または "空欄" なら html
+    // それ以外ならhref(リンク)を代入
+    let target = $(href == "#" || href == "" ? 'html' : href);
+
+    // トップからジャンプ先の要素までの距離を取得
+    let position = target.offset().top;
+
+    //scrollTop:要素の位置   までswing
+    $("html, body").animate({scrollTop:position}, 600, "swing");
+    return false;
   });
-
-  $('.top-btn').click(function () {
-    $('html, body').animate({ scrollTop: 0 }, 'slow');
-  });
-
 })
